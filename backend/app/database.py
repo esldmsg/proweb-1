0c19357101fe9956defe3298ce28ae10e29098bf
Balogun <sodiqb86@gmail.com>
2021-12-13 13:01:54 +1300

    commited

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,11 +1,18 @@
 import os
+import re
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
+#SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
+
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
+
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, echo= True
